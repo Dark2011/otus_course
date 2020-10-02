4d5c63f6f161a3c19b1e8ec4453a235817e985f4
dark2011 <ilya.zakharov@locotech-signal.ru>
2020-10-02 09:26:53 +0300

    add test with boost library

--- less1.1/src/helloworld.cpp
+++ less1.1/src/helloworld.cpp
@@ -1,9 +1,15 @@
 #include <version.h>
 
+#include <vector>
 
 int main(int argc, char* argv[])
 {
-	std::cout << version();
-	std::cout << "Hello world!";	
+	std::cout << "BUILD " << version() << std::endl;;
+	std::cout << "Hello, world!";	
+
+	std::vector<std::string> test;
+	test.push_back("123");
+
+	std::stoi(test[0]);
 	return 0;
 }
---
+++ less1.1/src/test_version.cpp
@@ -0,0 +1,28 @@
+#define BOOST_TEST_MODULE test_version
+
+#include <version.h>
+#include <boost/test/unit_test.hpp>
+#include <boost/tokenizer.hpp>
+#include <vector>
+
+
+BOOST_AUTO_TEST_SUITE(test_version)
+
+BOOST_AUTO_TEST_CASE(validate_test_version)
+{
+	std::string current_version = version();
+	using tokenizer = boost::tokenizer<boost::char_separator<char>> ;
+	tokenizer tok{current_version};
+	std::vector<std::string> res;
+	
+	for(auto&& token : tok)
+		res.push_back(token);
+	
+	const int VERSION_DIGIT_COUNT = 3;
+	BOOST_CHECK_EQUAL(res.size(), VERSION_DIGIT_COUNT, true);
+	//
+	const int VERSION_PATCH_POS = 2;	
+	BOOST_CHECK(std::stoi(res[VERSION_PATCH_POS]) > 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
