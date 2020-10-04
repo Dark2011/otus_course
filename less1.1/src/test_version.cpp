de5cf04876678f5f3b24097c44a6d3e5b2ac1695
dark2011 <ilya.zakharov@locotech-signal.ru>
2020-10-04 20:36:43 +0300

    change unit test cases

--- less1.1/src/test_version.cpp
+++ less1.1/src/test_version.cpp
@@ -1,22 +1,27 @@
 #define BOOST_TEST_MODULE test_version
-#define BOOST_TEST_LOG_LEVEL all
 
+// own
 #include <version.h>
+// boost
 #include <boost/test/unit_test.hpp>
 #include <boost/tokenizer.hpp>
+// std
 #include <vector>
 
 
 BOOST_AUTO_TEST_SUITE(test_version)
 
-BOOST_AUTO_TEST_CASE(validate_test_version)
+BOOST_AUTO_TEST_CASE(validate_version_call)
 {
 	std::string current_version = version();
-	BOOST_CHECK(current_version.length() > 0);
-	
-	BOOST_TEST_MESSAGE("Current version: ");
-	BOOST_TEST_MESSAGE(current_version);
-	
+	BOOST_CHECK(current_version.length() > 0);	
+}
+
+
+BOOST_AUTO_TEST_CASE(parsing_validate)
+{
+	std::string current_version = version();
+		
 	using separator = boost::char_separator<char>;
 	using tokenizer = boost::tokenizer<separator> ;
 	tokenizer tok{current_version, separator{"."}};
@@ -31,6 +36,23 @@ BOOST_AUTO_TEST_CASE(validate_test_version)
 	//
 	const int VERSION_PATCH_POS = 2;
 	//BOOST_TEST_MESSAGE("PATCH " << std::stoi(res[VERSION_PATCH_POS]) << std::endl); 
+	BOOST_CHECK(std::stoi(res[VERSION_PATCH_POS]) > 0);
+}
+
+
+BOOST_AUTO_TEST_CASE(validate_patch_version)
+{
+	std::string current_version = version();
+		
+	using separator = boost::char_separator<char>;
+	using tokenizer = boost::tokenizer<separator> ;
+	tokenizer tok{current_version, separator{"."}};
+	//
+	std::vector<std::string> res;	
+	for(auto&& token : tok)
+		res.push_back(token);	
+
+	const int VERSION_PATCH_POS = 2;
 	BOOST_CHECK(std::stoi(res[VERSION_PATCH_POS]) > 0);
 }
 
